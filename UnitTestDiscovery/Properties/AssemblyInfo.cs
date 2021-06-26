3477cac3117c61fbc3aede5177afd48da50927c7
mustafasilbir <silbirx@hotmail.com>
2021-06-27 00:51:55 +0300

    unit test

---
+++ UnitTestDiscovery/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestDiscovery")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestDiscovery")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1a267df2-1cc6-4513-8144-b18b09ae9f99")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestDiscovery/UnitTestMove.cs
@@ -0,0 +1,30 @@
+﻿using MstDiscovery;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MstDiscovery.Tests
+{
+    [TestClass()]
+    public class UnitTestMove
+    {
+        [TestMethod()]
+        public void MoveRovers()
+        {
+            RoverController rover = new RoverController();
+
+            string input = "5 5" + Environment.NewLine +
+                "1 2 N" + Environment.NewLine +
+                "LMLMLMLMM" + Environment.NewLine +
+                "3 3 E" + Environment.NewLine +
+                "MMRMMRMRRM";
+
+            string output= rover.MoveRovers(input);
+
+            string expectedOut = "1 3 N" + Environment.NewLine +
+            "5 1 E" + Environment.NewLine;
+
+            Assert.AreEqual(expectedOut, output);
+        }
+    }
+}
+ 
